62fe6ef381a86b909b24a6655e644fc6937150ec
István Antal <i@istvan.xyz>
2018-11-17 11:46:02 +0000

    Only run tslint if tsconfig exists

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,10 @@
 import { spawnSync } from 'child_process';
+import { existsSync } from 'fs';
+import { resolve } from 'path';
 
 export const test = () => {
     spawnSync('npm', ['audit'], { stdio: 'inherit', shell: true });
-    spawnSync('tslint', ['-c', 'tslint.json', '-p', 'tsconfig.json'], { stdio: 'inherit', shell: true });
+    if (existsSync(resolve(process.cwd(), 'tslint.json'))) {
+        spawnSync('tslint', ['-c', 'tslint.json', '-p', 'tsconfig.json'], { stdio: 'inherit', shell: true });
+    }
 };
