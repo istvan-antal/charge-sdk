6191fc8175b166e0aad917b1d4e4fde853cc934f
István Antal <i@istvan.xyz>
2018-11-18 08:31:30 +0000

    Add quotes

--- src/release.ts
+++ src/release.ts
@@ -1,6 +1,6 @@
 import { spawnSync } from 'child_process';
 
-spawnSync('npm', ['version', 'patch', '-m', '[Release] %s'], { stdio: 'inherit', shell: true });
+spawnSync('npm', ['version', 'patch', '-m', '"[Release] %s"'], { stdio: 'inherit', shell: true });
 spawnSync('npm', ['publish'], { stdio: 'inherit', shell: true });
 spawnSync('git', ['push'], { stdio: 'inherit', shell: true });
 spawnSync('git', ['push', '--tags'], { stdio: 'inherit', shell: true });
