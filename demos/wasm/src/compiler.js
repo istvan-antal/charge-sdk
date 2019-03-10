15d6f94e3c2609a1314c37641270ae33a555d28e
István Antal <i@istvan.xyz>
2019-03-10 08:18:53 +0000

    Fix wasm example

--- demos/wasm/src/compiler.js
+++ demos/wasm/src/compiler.js
@@ -1,15 +1,18 @@
 module.exports.default = (config) => ({
     ...config,
-    rules: [
-        ...config.rules,
-        {
-            test: /\.(c|cpp)$/,
-            use: {
-                loader: 'cpp-wasm-loader',
-                options: {
-                    asmJs: true,
+    module: {
+        ...config.module,
+        rules: [
+            ...config.rules,
+            {
+                test: /\.(c|cpp)$/,
+                use: {
+                    loader: 'cpp-wasm-loader',
+                    options: {
+                        asmJs: true,
+                    },
                 },
             },
-        },
-    ],
+        ],
+    },
 });
