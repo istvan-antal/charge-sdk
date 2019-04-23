c2c0e1bd9e0685c0c9362236e6973065a2a24849
István Antal <i@istvan.xyz>
2019-04-23 20:44:13 +0100

    Add some error handling to publish

--- src/release.ts
+++ src/release.ts
@@ -1,6 +1,9 @@
 import { spawnSync } from 'child_process';
 
-spawnSync('npm', ['version', 'patch', '-m', '"[Release] %s"'], { stdio: 'inherit', shell: true });
+const result = spawnSync('npm', ['version', 'patch', '-m', '"[Release] %s"'], { stdio: 'inherit', shell: true });
+if (result.status !== 0) {
+    process.exit(1);
+}
 spawnSync('npm', ['publish'], { stdio: 'inherit', shell: true });
 spawnSync('git', ['push'], { stdio: 'inherit', shell: true });
 spawnSync('git', ['push', '--tags'], { stdio: 'inherit', shell: true });
