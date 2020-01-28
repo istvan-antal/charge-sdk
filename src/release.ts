d77d2a084746d8e3657c5cdc0e0782916e6cac51
István Antal <i@istvan.xyz>
2020-01-28 06:15:22 +0000

    Move patch release task to package.json

--- src/release.ts
+++
@@ -1,9 +0,0 @@
-import { spawnSync } from 'child_process';
-
-const result = spawnSync('npm', ['version', 'patch', '-m', '"[Release] %s"'], { stdio: 'inherit', shell: true });
-if (result.status !== 0) {
-    process.exit(1);
-}
-spawnSync('npm', ['publish'], { stdio: 'inherit', shell: true });
-spawnSync('git', ['push'], { stdio: 'inherit', shell: true });
-spawnSync('git', ['push', '--tags'], { stdio: 'inherit', shell: true });
